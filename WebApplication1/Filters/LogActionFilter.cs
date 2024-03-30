7c11fde66fb60da7a92be6d34b1c9a3c817b8695
AlexeyGranddevs <165141315+AlexeyGranddevs@users.noreply.github.com>
2024-03-30 13:53:36 +0200

    Добавьте файлы проекта.

---
+++ WebApplication1/Filters/LogActionFilter.cs
@@ -0,0 +1,49 @@
+﻿
+namespace WebApplication1.Filters
+{
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System;
+    using System.IO;
+
+    public class LogActionFilter : IActionFilter
+    {
+        private readonly string _logFilePath;
+
+        public LogActionFilter()
+        {
+            // Получаем путь к папке Filters внутри директории приложения
+            string filtersFolder = Path.Combine(Directory.GetCurrentDirectory(), "Filters");
+
+            // Создаем папку Filters, если она не существует
+            if (!Directory.Exists(filtersFolder))
+            {
+                Directory.CreateDirectory(filtersFolder);
+            }
+
+            // Формируем путь к файлу внутри папки Filters
+            _logFilePath = Path.Combine(filtersFolder, "log.txt");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Запись информации в лог-файл
+            using (StreamWriter writer = new StreamWriter(_logFilePath, append: true))
+            {
+                writer.WriteLine($"Action method {context.ActionDescriptor.DisplayName} started at {DateTime.Now}");
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Запись информации в лог-файл
+            using (StreamWriter writer = new StreamWriter(_logFilePath, append: true))
+            {
+                writer.WriteLine($"Action method {context.ActionDescriptor.DisplayName} finished at {DateTime.Now}");
+            }
+        }
+    }
+
+
+}
+
+
---
+++ WebApplication1/Filters/UniqueUsersFilter.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApplication1.Filters
+{
+
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System;
+    using System.IO;
+
+        public class UniqueUsersFilter : IActionFilter
+        {
+            private readonly string _logFilePath;
+
+            public UniqueUsersFilter(string logFilePath)
+            {
+                _logFilePath = logFilePath;
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+                string ipAddress = context.HttpContext.Connection.RemoteIpAddress.ToString();
+                string logMessage = $"{DateTime.Now}: User with IP {ipAddress} accessed the application\n";
+
+                File.AppendAllText(_logFilePath, logMessage);
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+            {
+                // Do nothing
+            }
+       }
+    
+
+}
